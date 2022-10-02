e1f12339cc5f62411ec2b320b9f23432f92d6df5
Cheikh Tidiane Ndiaye <cheikhtiindiaye@gmail.com>
2022-10-02 17:38:01 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/models/employee';
+import { EmployeeService } from 'src/services/employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employeemanager-front';
+export class AppComponent implements OnInit{
+  public employees: Employee[] | undefined;
+  constructor(private employeeService: EmployeeService){}
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+  public getEmployees(): void{
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      }, 
+      (error : HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }
---
+++ src/models/employee.ts
@@ -0,0 +1,9 @@
+export interface Employee{
+    id: number;
+    name: string;
+    email: string;
+    job: string;
+    phone: string;
+    imageUrl: string;
+    employeeCode: string;
+}
